0d5c982df02d5cae633753ce39b4358446f90c00
Jouhar Kabeer <jouharkabeer412@gmail.com>
2025-07-07 12:39:44 +0530

    gg

--- src/api.js
+++ src/api.js
@@ -1,2 +1,3 @@
-export const Api = 'https://dsr-backend-rimy.onrender.com'
+// export const Api = 'https://dsr-backend-rimy.onrender.com'
 // export const Api = 'http://127.0.0.1:8000'
+export const Api = 'https://dsrbackend-production.up.railway.app'
