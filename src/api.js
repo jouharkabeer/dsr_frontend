3cc1d8344b5cd4419def6e7a5972b61057f6ba42
Jouhar Kabeer <jouharkabeer412@gmail.com>
2025-07-12 21:19:08 +0530

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
 // export const Api = 'https://lumberworld-backend.onrender.com'
-export const Api = 'https://dsr-backend-rimy.onrender.com/'
+export const Api = 'https://dsr-backend-rimy.onrender.com'
 // export const Api = 'https://dsrbackend-production.up.railway.app'
 // export const Api = 'http://localhost:8000'
